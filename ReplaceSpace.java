7670c5b208e25144bd4a1df4796f0e9e146d7e9a
Akshada0612 <155293231+Akshada0612@users.noreply.github.com>
2025-03-10 22:54:42 +0530

    Add files via upload

---
+++ ReplaceSpace.java
@@ -0,0 +1,19 @@
+//Relaces the places in string
+class ReplaceSpace
+{
+  public static void main(String[] args)
+{
+    String str = "Hello World";
+    String newStr = "";
+
+    for (int i = 0; i < str.length(); i++)
+	{
+      if (str.charAt(i) != ' ')
+	  {
+        newStr += str.charAt(i);
+      }
+    }
+
+    System.out.println(newStr);
+  }
+}
